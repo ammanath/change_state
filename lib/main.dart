2c5d75a6b841fe768cd7a9ca51b0b06b0aff0147
bit00 <akbrijesh@gmail.com>
2020-05-30 20:05:26 +0530

    added calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(ChangeColor());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Change State App",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: ChangeColor(),
+    );
+  }
+}
 
 class ChangeColor extends StatefulWidget {
   @override
@@ -8,16 +21,130 @@ class ChangeColor extends StatefulWidget {
 }
 
 class _ChangeColorState extends State<ChangeColor> {
+  int num1, num2, sum = 0;
+  final TextEditingController t1 = TextEditingController(text: '0');
+  final TextEditingController t2 = TextEditingController(text: '0');
+
+  void doAddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 + num2;
+    });
+  }
+
+  void doMult() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 * num2;
+    });
+  }
+
+  void doSub() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 - num2;
+    });
+  }
+
+  void doDiv() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 ~/ num2;
+    });
+  }
+
+void doClear() {
+  setState(() {
+  t1.text='0';
+  t2.text='0';
+    
+  });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Change state',
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Change'),
+          title: Text('Change1'),
         ),
-        body: Text('content'),
-      ),
-    );
-  }
+        body: Container(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Output : $sum ',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple),
+                ),
+                TextField(
+                    keyboardType: TextInputType.number,
+                    controller: t1,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Number 1',
+                    )),
+                TextField(
+                    keyboardType: TextInputType.number,
+                    controller: t2,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Number 2',
+                    )),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    MaterialButton(
+                      child: Text('+'),
+                      color: Colors.redAccent,
+                      onPressed: doAddition,
+                    ),
+                    MaterialButton(
+                      child: Text('-'),
+                      color: Colors.redAccent,
+                      onPressed: doSub,
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    MaterialButton(
+                      child: Text('*'),
+                      color: Colors.redAccent,
+                      onPressed: doMult,
+                    ),
+                    MaterialButton(
+                      child: Text('/'),
+                      color: Colors.redAccent,
+                      onPressed: doDiv,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    MaterialButton(
+                      child: Text('Clear'),
+                      color: Colors.redAccent,
+                      onPressed: doClear,
+                                          ),
+                                          
+                                        ],
+                                      ),
+                                    ],
+                                  )));
+                        }
+                      
+                        
 }
